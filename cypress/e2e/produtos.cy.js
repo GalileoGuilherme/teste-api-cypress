ff9b683b1d77db9a4f3bddc4d55dc6c4706d2345
Galileo Guilherme <galileoguilherme@outlook.com>
2022-10-01 20:50:14 -0300

    update

---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress" />
+
+describe('Teste da funcionalidade Produtos', () => {
+    let token
+
+    before(() => {
+        cy.token('fulano@qa.com', 'teste').then(tkn => { token = tkn })
+    });
+
+
+    it('Listar Produtos', () => {
+        cy.request({
+            method: 'GET',
+            url: 'http://localhost:3000/Produtos/'
+        }).then((Response) => {
+            expect(Response.body.produtos[1].nome).to.equal('Samsung 60 polegadas')
+            expect(Response.status).to.equal(200)
+            expect(Response.body).to.have.property('produtos')
+            expect(Response.duration).to.be.lessThan(20)
+        })
+    });
+
+    it('Cadastrar Produto', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3000/Produtos/',
+            body: {
+                "nome": "Relogio1",
+                "preco": 400,
+                "descricao": "acessório",
+                "quantidade": 80
+              },
+              headers: {autorization: token}
+           //Cadastrar produto novo   
+        }).then((response) => {
+            expect(response.status).to.equal(201)
+            expect(response.body.message).to.equal("Cadastro realizado com sucesso") 
+        })
+
+
+    });
+
+});
